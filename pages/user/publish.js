66fca742141ec00304f183b73241784d379da69c
Paulo Júnior <paulojrcosta300@gmail.com>
2023-03-08 08:58:30 -0300

    Category field validation

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -15,6 +15,8 @@ import {
   InputLabel,
   OutlinedInput,
   InputAdornment,
+  MenuItem,
+  FormHelperText,
 }from '@material-ui/core'
 import { makeStyles } from '@material-ui/core'
 import { DeleteForever } from '@material-ui/icons'
@@ -92,6 +94,7 @@ const validationSchema = yup.object().shape({
     .min(6, 'Escreva um título maior')
     .max(100, 'Título muito grande')
     .required('Campo obrigatório'),
+  category: yup.string().required('Campo obrigatório'),
 })
 
 const Publish = () => {
@@ -127,7 +130,7 @@ const Publish = () => {
       <Formik
         initialValues={{
           title: '',
-
+          category: '',
         }}
         validationSchema={validationSchema}
         onSubmit={(values) => {
@@ -174,32 +177,33 @@ const Publish = () => {
                   <Typography component='h6' variant='h6' color='textPrimary' gutterBottom>
                     Categoria
                   </Typography>
-                  <Select
-                    native
-                    value=''
-                    fullWidth
-                    onChange={() => {}}
-                    inputProps={{
-                      name: 'age',
-                    }}
-                  >
-                    <option value="">Selecione</option>
-                    <option value={1}>Bebê e Crianças</option>
-                    <option value={2}>Agriculturas</option>
-                    <option value={3}>Moda</option>
-                    <option value={3}>Carros, Motos e Barcos</option>
-                    <option value={3}>Serviços</option>
-                    <option value={3}>Lazer</option>
-                    <option value={3}>Animais</option>
-                    <option value={3}>Moveis, Casa e Jardim</option>
-                    <option value={3}>Imóveis</option>
-                    <option value={3}>Equipamentos e Ferramentas</option>
-                    <option value={3}>Celulares e Tablets</option>
-                    <option value={3}>Esporte</option>
-                    <option value={3}>Tecnologia</option>
-                    <option value={3}>Emprego</option>
-                    <option value={3}>Outros</option>
-                  </Select>
+                  <FormControl error={errors.category} fullWidth>
+                    <Select
+                      name='category'
+                      value={values.category}
+                      fullWidth
+                      onChange={handleChange}
+                    >
+                      <MenuItem value="Bebê e Crianças">Bebê e Crianças</MenuItem>
+                      <MenuItem value="Agriculturas">Agriculturas</MenuItem>
+                      <MenuItem value="Moda">Moda</MenuItem>
+                      <MenuItem value="Carros, Motos e Barcos">Carros, Motos e Barcos</MenuItem>
+                      <MenuItem value="Serviços">Serviços</MenuItem>
+                      <MenuItem value="Lazer">Lazer</MenuItem>
+                      <MenuItem value="Animais">Animais</MenuItem>
+                      <MenuItem value="Moveis, Casa e Jardim">Moveis, Casa e Jardim</MenuItem>
+                      <MenuItem value="Imóveis">Imóveis</MenuItem>
+                      <MenuItem value="Equipamentos e Ferramentas">Equipamentos e Ferramentas</MenuItem>
+                      <MenuItem value="Celulares e Tablets">Celulares e Tablets</MenuItem>
+                      <MenuItem value="Esporte">Esporte</MenuItem>
+                      <MenuItem value="Tecnologia">Tecnologia</MenuItem>
+                      <MenuItem value="Emprego">Emprego</MenuItem>
+                      <MenuItem value="Outros">Outros</MenuItem>
+                    </Select>
+                    <FormHelperText>
+                      {errors.category}
+                    </FormHelperText>
+                  </FormControl>
                 </Box>
               </Container>
 
